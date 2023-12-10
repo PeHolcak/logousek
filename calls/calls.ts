5b2fdcaff8420d250a993596bfd09e6443a362ae
Petr Holcak <petrholcak@gmail.com>
2023-12-10 22:49:27 +0100

    Remove .env

--- calls/calls.ts
+++ calls/calls.ts
@@ -2,6 +2,5 @@ import axios from 'axios'
 
 export const callApi = async <T>(endpoint: string, data: object, method: "POST" | "GET") => {
     const usedFunction = method === "GET" ? axios.get : axios.post
-    console.log("process.env.URL", `${process.env.NEXT_PUBLIC_URL}/api/${endpoint}`)
-    return await usedFunction<T>(`${process.env.NEXT_PUBLIC_URL}/api/${endpoint}`, data)
+    return await usedFunction<T>(`${process.env.NEXT_PUBLIC_URL}api/${endpoint}`, data)
 }
